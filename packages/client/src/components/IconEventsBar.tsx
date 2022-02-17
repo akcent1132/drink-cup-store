668bc638fb30094979377332bd56ad1ea6c9dfbe
Ivan <grom1132@mail.ru>
2022-02-17 18:06:13 +0100

    IconEvetBar: Implement basic icon arrange

--- packages/client/src/components/IconEventsBar.tsx
+++ packages/client/src/components/IconEventsBar.tsx
@@ -1,12 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import styled from "@emotion/styled";
 import "../index.css";
 import { css, useTheme } from "@emotion/react";
 import { timeFormat } from "d3-time-format";
 import { timeYear } from "d3-time";
-import { scaleTime } from "d3-scale";
+import { ScaleTime, scaleTime } from "d3-scale";
 import { extent } from "d3-array";
 import { ReactComponent as IconAmendments } from "../assets/foodIcons/noun-cooperative-4476250.svg";
 import { ReactComponent as IconWeeding } from "../assets/foodIcons/noun-hand-weeding-4475618.svg";
@@ -16,6 +16,7 @@ import { ReactComponent as IconTillage } from "../assets/foodIcons/noun-till-amo
 import { ReactComponent as IconIrrigation } from "../assets/foodIcons/noun-water-drop-4476116.svg";
 import { ReactComponent as IconJoker } from "../assets/foodIcons/noun-indigenous-knowledge-4476235.svg";
 import useSize from "@react-hook/size";
+import { forceCenter, forceCollide, forceSimulation, forceY } from "d3-force";
 
 export const defaultTheme = {
   iconSize: 26,
@@ -59,6 +60,15 @@ export type FarmEvent = {
   date: Date;
 };
 
+type IconNode = {
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  targetX?: number;
+  event: FarmEvent;
+};
+
 export const defaultKnobs = Object.freeze({
   middleLineWidth: 3,
   middleLineColor: "white",
@@ -87,6 +97,22 @@ const drawTickText = (
   ctx.restore();
 };
 
+const dateForce = (() => {
+  let nodes: IconNode[] = [];
+  let scale: ScaleTime<number, number, never>;
+  const force = (alpha: number) => {
+    if (!scale) return;
+
+    nodes.map((node) => {
+      node.vx += (scale(node.event.date) - node.x) * alpha;
+      node.targetX = scale(node.event.date);
+    });
+  };
+  force.initialize = (n: IconNode[]) => (nodes = n);
+  force.setScale = (n: ScaleTime<number, number, never>) => (scale = n);
+  return force;
+})();
+
 /**
  * Primary UI component for user interaction
  */
@@ -94,8 +120,15 @@ export const EventsBar = (props: Props) => {
   const events = props.events || [];
   const ref = useRef<HTMLDivElement>(null);
   const [width, height] = useSize(ref);
-  console.log({ width, height });
   const theme = useTheme().iconEventsBar;
+  const simulation = useMemo(
+    () =>
+      forceSimulation<IconNode>([])
+        .force("y", forceY(30))
+        .force("date", dateForce)
+        .force("radius", forceCollide(theme.iconSize*0.55)),
+    []
+  );
   const scale = useMemo(() => {
     const [first, last] = extent(events.map((e) => e.date));
     const start = timeYear.floor(first || new Date());
@@ -103,23 +136,51 @@ export const EventsBar = (props: Props) => {
 
     const xStart = theme.middleLineMargin;
     const xEnd = (width || 100) - theme.middleLineMargin;
-    console.log({ start, end, xStart, xEnd });
-    return scaleTime().domain([start, end]).range([xStart, xEnd]);
+    const scale = scaleTime().domain([start, end]).range([xStart, xEnd]);
+    dateForce.setScale(scale);
+    simulation.tick(100).alpha(1).restart();
+    return scale;
   }, [width, theme.middleLineMargin]);
+  const [nodes, setNodes] = useState<IconNode[]>([]);
 
+  // add events to the simulation
+  useEffect(() => {
+    const n = simulation.nodes;
+    simulation.nodes(
+      events.map((event) => ({
+        x: scale(event.date),
+        y: 30,
+        vx: 0,
+        vy: 0,
+        event,
+      }))
+    );
+    // simulation.restart();
+  }, [events]);
+  useEffect(() => {
+    simulation.on("tick", () => {
+      setNodes(simulation.nodes());
+    });
+  }, []);
+  nodes.forEach(({ event, x, y, vx, targetX }) =>
+    console.log({ x, y, vx, targetX })
+  );
+  console.log("sim alpha", simulation.alpha())
   return (
     <Bar ref={ref}>
-      {events.map((event) => {
+      {nodes.map(({ event, x, y }) => {
         const iconProps = {
           css: css`
             position: absolute;
-            left: ${scale(event.date)}px;
+            left: ${x}px;
+            top: ${y}px;
           `,
           width: theme.iconSize,
           height: theme.iconSize,
           fill: "white",
           title: event.type,
         };
+
         const Icon = getEventIcon(event.type);
         return <Icon {...iconProps} />;
       })}
