36c4ad165d1e0115ff5ffaa7dafc137ae95034ff
Ivan <grom1132@mail.ru>
2022-07-13 14:25:42 +0200

    Update graphql function

--- packages/server/src/lambda/graphql.ts
+++ packages/server/src/lambda/graphql.ts
@@ -1,10 +1,19 @@
-
-const { ApolloServer, gql } = require("apollo-server-lambda");
+const { ApolloServer } = require("apollo-server-lambda");
 import { typeDefs, resolvers } from "../schema/schema";
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  introspection: true,
+  playground: true,
 });
 
-exports.handler = server.createHandler();
+const getHandler = (event: any, context: any) => {
+  const graphqlHandler = server.createHandler();
+  if (!event.requestContext) {
+    event.requestContext = context;
+  }
+  return graphqlHandler(event, context);
+};
+
+exports.handler = getHandler;
