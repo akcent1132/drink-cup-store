feb09d833e298b37e367f4e477581ec63bc2bb77
Ivan <grom1132@mail.ru>
2022-08-24 19:13:15 +0200

    Update date format

--- packages/client/src/graphql/loaders/farmEvents.ts
+++ packages/client/src/graphql/loaders/farmEvents.ts
@@ -3,7 +3,14 @@ import pMemoize from "p-memoize";
 import { surveyStackApiUrl } from "../../utils/env";
 import { PlantingEventDetail } from "../resolvers.generated";
 
-const formatValue = (value: any) => isObject(value) ? JSON.stringify(value, null, 2) : toString(value);
+const formatValue = (value: any) =>
+  isObject(value)
+    ? JSON.stringify(value, null, 2)
+    : !Number.isNaN(Date.parse(value))
+    ? `${new Date(value).toLocaleDateString()} ${new Date(
+        value
+      ).toLocaleTimeString()}`
+    : toString(value);
 
 // Fetch all the event details for a planting
 export const loadEventDetails = pMemoize(
@@ -21,7 +28,9 @@ export const loadEventDetails = pMemoize(
     return Object.entries(externalData)
       .filter(
         ([key, value]) =>
-          !isNil(value) && (!Array.isArray(value) || value.length > 0) && key !== 'drupal_uid'
+          !isNil(value) &&
+          (!Array.isArray(value) || value.length > 0) &&
+          key !== "drupal_uid"
       )
       .map(([key, value]) => ({
         __typename: "PlantingEventDetail" as "PlantingEventDetail",
